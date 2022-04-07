69308eed06483a956aace0b076e4c596a940be55
Fernando González <fegoa89@gmail.com>
2022-04-07 18:39:40 +0200

    using ThreadPoolExecutor to extract source download link

--- pypisd/main.py
+++ pypisd/main.py
@@ -1,20 +1,33 @@
 import requests
-from bs4 import BeautifulSoup
-import threading
 import subprocess
+import concurrent.futures
+from bs4 import BeautifulSoup
 
-global_lock = threading.Lock()
+def get_source_distribution_link_for_library(library, timeout=10):
+    if len(library) == 2:
+        url = f"https://pypi.org/project/{library[0]}/{library[1]}/#files"
+    else:
+        url = f"https://pypi.org/project/{library[0]}/#files"
+
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    get_download_link_div = soup.find("div", {"class": "card file__card"})
+
+
+    if get_download_link_div:
+        return [library[0],library[1] if (len(library) == 2) else "using latest version",soup.find("div", {"class": "card file__card"}).find("a")["href"]]
+    else:
+        return [f"Can not find download link for {library[0]}, version {library[1]}"]
 
 def cli():
     lib_list_bytes = get_pip_list_stdout()
     lib_list = extract_lib_list_from_bytes_output(lib_list_bytes)
-    threads = []
-
-    for library in lib_list:
-        t = threading.Thread(target=get_source_distribution_link_for_library(library))
-        threads.append(t)
-        t.start()
-    [thread.join() for thread in threads]
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for library in lib_list:
+            futures.append(executor.submit(get_source_distribution_link_for_library, library=library))
+        for future in concurrent.futures.as_completed(futures):
+            print(future.result())
 
 def get_pip_list_stdout() -> bytes:
     pip_freeze_process = subprocess.Popen(['pip', 'list'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -28,20 +41,3 @@ def extract_lib_list_from_bytes_output(pip_stdout: bytes) -> list:
             lib_list.append(line)
 
     return lib_list
-
-
-def get_source_distribution_link_for_library(library: list):
-    if len(library) == 2:
-        url = f"https://pypi.org/project/{library[0]}/{library[1]}/#files"
-    else:
-        url = f"https://pypi.org/project/{library[0]}/#files"
-
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    get_download_link_div = soup.find("div", {"class": "card file__card"})
-
-    with open("thread_writes", "a+") as file:
-        if get_download_link_div:
-            file.write(f'{library[0]},{library[1] if (len(library) == 2) else "using latest version"},{soup.find("div", {"class": "card file__card"}).find("a")["href"]}\n')
-        else:
-            file.write(f"Can not find download link for {library[0]}, version {library[1]}\n")
--- tests/test_pypisd.py
+++ tests/test_pypisd.py
@@ -1,5 +1,9 @@
 from pypisd import __version__
-
+import subprocess
+from pypisd.main import cli
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_pypisd():
+    subprocess.run(["pypisd"], capture_output=True)
