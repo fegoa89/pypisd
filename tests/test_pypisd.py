9b73eb0fc3a2b989ea75038b668d6d6053b8c80c
Fernando González <fegoa89@gmail.com>
2022-04-09 11:00:14 +0200

    WIP setting up fixtures

--- tests/test_pypisd.py
+++ tests/test_pypisd.py
@@ -1,9 +1,22 @@
-from pypisd import __version__
-import subprocess
+import pytest
+from unittest import mock
 from pypisd.main import cli
 
-def test_version():
-    assert __version__ == '0.1.0'
+pip_list_output = b'Package            Version\n------------------ ---------\nrequests           2.27.1\n'
 
-def test_pypisd():
-    subprocess.run(["pypisd"], capture_output=True)
+@pytest.fixture(autouse=True)
+def mock_pip_list_command():
+    with mock.patch('pypisd.main.Popen') as mocked:
+        mocked.return_value.returncode = 0
+        mocked.return_value.communicate.return_value = (pip_list_output, None)
+        yield mocked
+
+@pytest.fixture(autouse=True)
+def mock_pypi_request_fetch_page():
+    library_html_page_response = open('tests/mock_pypi_library_page_response.html', 'r', encoding='utf-8').read()
+    mock_get_request = mock.Mock(side_effect = lambda k:{}.get(k, mock.Mock(text=library_html_page_response)))
+    with mock.patch('pypisd.main.requests.get', mock_get_request) as mocked:
+        yield mocked
+
+def test_pypisd_file_output_header_generation():
+    cli()
