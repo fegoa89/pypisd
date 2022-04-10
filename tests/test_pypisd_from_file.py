0805b2c977cf8a2d69c48899dd6744bc0838d363
Fernando González <fegoa89@gmail.com>
2022-04-10 22:42:49 +0200

    adding test to input_file case

--- tests/test_pypisd_from_file.py
+++ tests/test_pypisd_from_file.py
@@ -1,5 +1,5 @@
 
-import sys, os
+import os
 import csv
 import argparse
 import pytest
@@ -16,7 +16,7 @@ def mock_pypi_request_fetch_page():
 
 @pytest.fixture()
 def requirements_input_file():
-    requirements_file_path = f"{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}/tests/test_requirements_file.txt"
+    requirements_file_path = f"{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}/tests/test_input_file.txt"
     with mock.patch('pypisd.main.argparse.ArgumentParser.parse_args', return_value=argparse.Namespace(input_file=requirements_file_path, output_file='pypi_sd_links_from_file.csv')):
         yield mock
 
@@ -26,3 +26,34 @@ def test_pypisd_file_output_header_generation(requirements_input_file):
     with open('pypi_sd_links_from_file.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         assert reader.fieldnames == ["library_name", "version", "license", "source_distribution_link"]
+
+
+def test_pypisd_file_test_library_row_content(requirements_input_file):
+    cli()
+    with open('pypi_sd_links_from_file.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        reader_list = list(reader)
+        # license and source_distribution_link are the same since we
+        # are mocking the requests
+        reader_list = sorted(reader_list, key = lambda i: i['library_name'])
+
+        assert reader_list[0]["library_name"] == "Click"
+        assert reader_list[0]["version"] == "7.0"
+        assert reader_list[1]["library_name"] == "Flask"
+        assert reader_list[1]["version"] == "using latest version"
+        assert reader_list[2]["library_name"] == "beautifulsoup4"
+        assert reader_list[2]["version"] == "4.6.3"
+        assert reader_list[3]["library_name"] == "bleach"
+        assert reader_list[3]["version"] == "2.1.4"
+        assert reader_list[4]["library_name"] == "certifi"
+        assert reader_list[4]["version"] == "2018.8.24"
+        assert reader_list[5]["library_name"] == "chardet"
+        assert reader_list[5]["version"] == "3.0.4"
+        assert reader_list[6]["library_name"] == "cycler"
+        assert reader_list[6]["version"] == "0.10.0"
+        assert reader_list[7]["library_name"] == "decorator"
+        assert reader_list[7]["version"] == "4.3.0"
+        assert reader_list[8]["library_name"] == "defusedxml"
+        assert reader_list[8]["version"] == "0.5.0"
+        assert reader_list[9]["library_name"] == "requests"
+        assert reader_list[9]["version"] == "2.27.1"
